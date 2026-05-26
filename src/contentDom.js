53e19f1f0f0cf0ae5e6c5366a062d97429a057b2
JunWeiLi233 <mcpejunwei@gmail.com>
2026-05-26 00:06:20 -0400

    Match hyphenated RMP course numbers

--- src/contentDom.js
+++ src/contentDom.js
@@ -6,7 +6,7 @@ const COMMENT_PREVIEW_LENGTH = 150;
 const MAX_RENDERED_COMMENTS = 3;
 const DEFAULT_RMP_URL = "https://www.ratemyprofessors.com/";
 const PLACEHOLDER_COMMENT_TEXT = new Set(["n/a", "na", "none", "no comment", "no comments", "no comments yet"]);
-const COURSE_CODE_PATTERN = /\b([A-Z]{2,5}-[A-Z]{2}\s*\d{3,4})\b/i;
+const COURSE_CODE_PATTERN = /\b([A-Z]{2,5}-[A-Z]{2}[-\s]*\d{3,4})\b/i;
 const SPACED_COURSE_CODE_PATTERN = /\b([A-Z]{2,5}\s+[A-Z]{2}\s*0?\d{3,4})\b/i;
 const COMPACT_COURSE_CODE_PATTERN = /\b([A-Z]{2,5}[A-Z]{2}0?\d{3,4})\b/i;
 const CONTROLLED_OPTION_SELECTOR = "[role='option'], [aria-selected], [aria-checked], [aria-current], [aria-pressed], [data-selected], [data-active], [data-checked], [data-current], [data-focus], [data-focused], [data-highlighted], [data-pressed], [data-state], [selected], [class]";
@@ -947,7 +947,7 @@ function normalizeCourseCode(value) {
     .toUpperCase()
     .replace(/\b([A-Z]{2,5})\s+([A-Z]{2})\s*0*(\d{1,4})\b/, (_match, subject, school, courseNumber) => `${subject}-${school} ${Number(courseNumber)}`)
     .replace(/\b([A-Z]{2,5})([A-Z]{2})0*(\d{1,4})\b/, (_match, subject, school, courseNumber) => `${subject}-${school} ${Number(courseNumber)}`)
-    .replace(/\b([A-Z]{2,5}-[A-Z]{2})\s*0*(\d{1,4})\b/, (_match, prefix, courseNumber) => `${prefix} ${Number(courseNumber)}`);
+    .replace(/\b([A-Z]{2,5}-[A-Z]{2})[-\s]*0*(\d{1,4})\b/, (_match, prefix, courseNumber) => `${prefix} ${Number(courseNumber)}`);
 }
 
 function uniqueNames(names) {
